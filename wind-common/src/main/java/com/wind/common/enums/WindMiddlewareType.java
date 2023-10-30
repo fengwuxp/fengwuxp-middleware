136a5e385a435feebd435c2d0aa46ffb58669264
伍枭鹏 <1109695647@qq.com>
2023-10-30 12:52:53 +0800

    feature：WindMiddlewareType#WIND 中间件类型配置

--- wind-common/src/main/java/com/wind/common/enums/WindMiddlewareType.java
+++ wind-common/src/main/java/com/wind/common/enums/WindMiddlewareType.java
@@ -1,8 +1,10 @@
 package com.wind.common.enums;
 
+import com.wind.common.WindConstants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static com.wind.common.WindConstants.WIND_ELASTIC_JOB_NAME;
 import static com.wind.common.WindConstants.WIND_MYSQL_NAME;
 import static com.wind.common.WindConstants.WIND_OSS_NAME;
 import static com.wind.common.WindConstants.WIND_REDIS_NAME;
@@ -18,21 +20,38 @@ import static com.wind.common.WindConstants.WIND_ROCKETMQ_NAME;
 @Getter
 public enum WindMiddlewareType implements DescriptiveEnum {
 
+    /**
+     * wind
+     */
+    WIND(WindConstants.WIND.toLowerCase(), WindConstants.WIND),
 
     /**
-     * 一般是 Mysql
+     * 数据库
+     * Mysql
+     * https://www.mysql.com/cn/
      */
     MYSQL(WIND_MYSQL_NAME, "数据库"),
 
     /**
      * redis 缓存
+     * https://redis.io/
+     * redisson: https://github.com/redisson/redisson
      */
     REDIS(WIND_REDIS_NAME, "REDIS"),
 
     /**
      * 消息队列
+     * rocketmq
+     * https://rocketmq.apache.org/zh/
      */
     ROCKETMQ(WIND_ROCKETMQ_NAME, "ROCKETMQ"),
+
+    /**
+     * 分布式定时任务调度
+     * elasticjob
+     * https://shardingsphere.apache.org/elasticjob/
+     */
+    ELASTIC_JOB(WIND_ELASTIC_JOB_NAME, "ELASTIC-JOB"),
 
     /**
      * 对象存储
