b9149aa33bc3ed717df21ef52b58819bac07fc76
伍枭鹏 <1109695647@qq.com>
2024-01-03 15:41:34 +0800

    chore(k8s)：调整优雅停机时长

--- k8s/docker/pre-stop.sh
+++ k8s/docker/pre-stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 等待 5s ，给 k8s 足够的时间摘流
-sleep 5
+# 等待 20s ，给 k8s 足够的时间摘流以及应用处理未完成的请求
+sleep 20
 echo "exec graceful shutdown"
 # springboot 优雅停机
 curl -X POST 127.0.0.1:8899/actuator/shutdown
 [ $? -eq 0 ] && echo "springboot 优雅关闭"
-sleep 3
+sleep 5
