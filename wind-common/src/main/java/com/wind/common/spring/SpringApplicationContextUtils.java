2c9a20f31fd76e993539a309bf0aef6aa8fc287c
wuxp <1109695647@qq.com>
2024-06-13 06:41:53 +0800

    fix：SpringApplicationContextUtils 在应用启动后不在允许重设 ApplicationContext

--- wind-common/src/main/java/com/wind/common/spring/SpringApplicationContextUtils.java
+++ wind-common/src/main/java/com/wind/common/spring/SpringApplicationContextUtils.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.lang.NonNull;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -22,6 +24,8 @@ public class SpringApplicationContextUtils implements ApplicationContextAware {
 
     private static final AtomicReference<ApplicationContext> CONTEXT_HOLDER = new AtomicReference<>();
 
+    private static final AtomicBoolean STARTED = new AtomicBoolean(false);
+
     public static String getProperty(String key) {
         return requireApplicationContext().getEnvironment().getProperty(key, String.class);
     }
@@ -29,7 +33,18 @@ public class SpringApplicationContextUtils implements ApplicationContextAware {
     @Override
     public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
         AssertUtils.notNull(applicationContext, "argument applicationContext must not null");
+        if (CONTEXT_HOLDER.get() != null && STARTED.get()) {
+            log.error("reset spring application context, old contextType = {}, start date = {}, new contextType = {}, start date = {}",
+                    CONTEXT_HOLDER.get().getClass().getName(), new Date(CONTEXT_HOLDER.get().getStartupDate()),
+                    applicationContext.getClass().getName(), new Date(applicationContext.getStartupDate()));
+            return;
+        }
+        log.info("set spring application context, contextType = {}, start date = {}", applicationContext.getClass().getName(), new Date(applicationContext.getStartupDate()));
         CONTEXT_HOLDER.set(applicationContext);
+    }
+
+    public static void markStarted() {
+        STARTED.set(true);
     }
 
     public static void publishEvent(ApplicationEvent event) {
--- wind-server/src/main/java/com/wind/server/initialization/WindApplicationStartedListener.java
+++ wind-server/src/main/java/com/wind/server/initialization/WindApplicationStartedListener.java
@@ -1,5 +1,6 @@
 package com.wind.server.initialization;
 
+import com.wind.common.spring.SpringApplicationContextUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -30,6 +31,8 @@ public class WindApplicationStartedListener implements ApplicationListener<Appli
         if (flag.get()) {
             return;
         }
+        // 标记应用已启动
+        SpringApplicationContextUtils.markStarted();
         // 执行系统初始化器
         execSystemInitializers(event.getApplicationContext());
     }
