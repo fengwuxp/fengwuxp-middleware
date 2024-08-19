42c46176585df8ae692dbbbd13cd8ef2c2bbcefb
wuxp <1109695647@qq.com>
2024-08-19 14:47:15 +0800

    chore(docker)：镜像支持 jvm 线程状态 dump 支持

--- k8s/docker/pre-stop.sh
+++ k8s/docker/pre-stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# dump jvm 线程状态
+timestamp=`date +%s`
+datetime=`date +%F`
+[ ! -d /data/jvm-dump/$SPRING_APPLICATION_NAME/$datetime ] && mkdir -p /data/jvm-dump/$JVM_DUMP_DIR/$SPRING_APPLICATION_NAME/$datetime
+jstack -l 1 > /data/jvm-dump/$JVM_DUMP_DIR/$SPRING_APPLICATION_NAME/$datetime/thread-$POD_NAME-$timestamp.log
+
 # 等待 20s ，给 k8s 足够的时间摘流以及应用处理未完成的请求
 sleep 20
 echo "exec graceful shutdown"
